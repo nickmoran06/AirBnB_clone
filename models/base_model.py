ed4efdbbc5c9c82baefdb1bc0bb751358654fa6d
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-11-08 23:59:42 +0000

    [Feature] method to generate a dictionary representation of an instance

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,18 @@ class BaseModel:
     """
     BaseModel is the base class of the project
     """
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, date))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the class"""
