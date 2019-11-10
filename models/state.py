33f01e6eccef7fd6e19a49be88bf5b7a0568ee20
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-11-10 01:00:02 +0000

    [Feature] Classes that inherit from the BaseModel to represent the program

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module with the State class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State representation
+    """
+
+    name = ""
