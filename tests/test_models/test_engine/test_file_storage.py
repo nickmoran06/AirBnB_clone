e25890349520b6da1cfdf1f650bc7ce104e660eb
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-11-13 20:59:07 +0000

    [Feature] Adding unitest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module of the BaseModel unittest
+Module of the Amenity unittest
 """
 
 import unittest
@@ -8,11 +8,12 @@ from models.base_model import BaseModel
 from datetime import datetime
 import os
 import pep8
+from models import storage
 
 
-class TestBaseModel(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """
-    Test of the BaseModel class
+    Test of the FileStorage class
     """
 
 
@@ -20,7 +21,7 @@ class TestBaseModel(unittest.TestCase):
     @classmethod
     def SetUp(cls):
         """Instance of the class"""
-        cls.inst = BaseModel()
+        cls.inst = Amenity()
 
     @classmethod
     def Teardown(cls):
@@ -35,33 +36,26 @@ class TestBaseModel(unittest.TestCase):
 
     #Functionality
     def TestAtributtesClass(self):
-        self.inst.name = "Diego"
-        self.inst.number = 19
-        self.inst.place = "San Francisco"
-        self.inst.number_rooms = 1
+        self.inst.name = "Nicolas"
         self.assertEqual(str, type(self.inst.name))
-        self.assertEqual(int, type(self.inst.number))
-        self.assertEqual(str, type(self.inst.place))
-        self.assertEqual(int, type(self.inst.number_rooms))
-        self.assertEqual(datetime.now(), self.inst.created_at)
-        self.assertEqual(datetime.now(), self.inst.updated_at)
+        self.assertEqual("Nicolas", self.inst.name)
 
 
     #Documentation
     def TestModuleDocstring(self):
         """Testing the documentation of the module"""
-        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(Amenity.__doc__)
 
     def TestMethodsDocstring(self):
         """Testing the documentation of the different methods"""
-        for doc in dir(BaseModel):
+        for doc in dir(Amenity):
             self.assertIsNotNone(doc.__doc__)
 
 
     #Existence and types
     def TestIsInstance(self):
         """Testing the existence of the instance"""
-        self.assertIsInstance(self.inst, BaseModel)
+        self.assertIsInstance(self.inst, Amenity)
 
     def TestTypeId(self):
         """Test the type of the method id"""
@@ -74,18 +68,19 @@ class TestBaseModel(unittest.TestCase):
 
     def TestMethods(self):
         """Testing the existence of the different methods"""
-        self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "__str__"))
-        self.assertTrue(hasattr(BaseModel, "save"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
+        self.assertTrue(hasattr(Amenity, "__init__"))
+        self.assertTrue(hasattr(Amenity, "__str__"))
+        self.assertTrue(hasattr(Amenity, "save"))
+        self.assertTrue(hasattr(Amenity, "to_dict"))
 
     def TestClassDict(self):
         """Testing the dictionary of the class"""
         ClassDict = self.inst.to_dict()
+        self.assertEqual(dict, type(dict_test))
         self.assertIsInstance(ClassDict["created_at"], str)
         self.assertIsInstance(ClassDict["updated_at"], str)
         self.assertTrue("ClassDict" in dir(self.inst))
-        self.assertEqual(test_dict.__class_.__name__, "BaseModel")
+        self.assertEqual(Classict.__class_.__name__, "Amenity")
 
 if __name__ == "__main__":
     unittest.main()
