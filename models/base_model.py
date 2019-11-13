f1a50847b1f8b4c35ddb8fd29ee05d28b14cd67e
nickmoran06 <nicolasmoranpulido@gmail.com>
2019-11-13 06:41:08 +0000

    [Feature] Adding of a new class also the storage of a new variable in the base

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """String representation of the class"""
